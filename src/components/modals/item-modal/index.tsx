8f79b56be1f237d1390a777b50c706be42c36c98
juliswer <julianswer36@gmail.com>
2022-10-22 13:42:44 -0300

    fix(): fixed animation bug on home page

--- src/components/modals/item-modal/index.tsx
+++ src/components/modals/item-modal/index.tsx
@@ -9,60 +9,44 @@ type Props = {
 };
 
 const PlaceBidModal = ({ show, handleModal, item }: Props) => (
-  <Modal
-    className="rn-popup-modal placebid-modal-wrapper"
-    show={show}
-    onHide={handleModal}
-    centered
-  >
-    {show && (
-      <button type="button" className="btn-close" aria-label="Close" onClick={handleModal}>
-        <i className="feather-x" />
-      </button>
-    )}
-    <Modal.Header>
-      <h3 className="modal-title fw-light">
-        Buy{' '}
-        <span className="fw-bold">
-          {item?.name}#{item?.tokenId}
-        </span>{' '}
-        NFT
-      </h3>
-    </Modal.Header>
-    <Modal.Body>
-      <p className="text-center">
-        You are about to purchase an NFT to <span className="fw-bold">{item?.author.address}</span>
-      </p>
-      <div className="placebid-form-box">
-        {/* <div className="bid-content">
-          <div className="bid-content-top">
-            <div className="bid-content-left">
-              <input id="value" type="text" name="value" />
-              <span>wETH</span>
+  <div>
+    {item ? (
+      <Modal
+        className="rn-popup-modal placebid-modal-wrapper"
+        show={show}
+        onHide={handleModal}
+        centered
+      >
+        {show && (
+          <button type="button" className="btn-close" aria-label="Close" onClick={handleModal}>
+            <i className="feather-x" />
+          </button>
+        )}
+        <Modal.Header>
+          <h3 className="modal-title fw-light">
+            Buy{' '}
+            <span className="fw-bold">
+              {item?.name}#{item?.tokenId}
+            </span>{' '}
+            NFT
+          </h3>
+        </Modal.Header>
+        <Modal.Body>
+          <p className="text-center">
+            You are about to purchase an NFT to{' '}
+            <span className="fw-bold">{item.owner?.address}</span>{' '}
+          </p>
+          <div className="placebid-form-box">
+            <div className="bit-continue-button">
+              <Button path="/connect" size="medium" fullwidth>
+                Buy NFT for {item?.price} ETH
+              </Button>
             </div>
           </div>
-
-          <div className="bid-content-mid">
-            <div className="bid-content-left">
-              <span>Your Balance</span>
-              <span>Service fee</span>
-              <span>Total bid amount</span>
-            </div>
-            <div className="bid-content-right">
-              <span>9578 wETH</span>
-              <span>10 wETH</span>
-              <span>9588 wETH</span>
-            </div>
-          </div>
-        </div> */}
-        <div className="bit-continue-button">
-          <Button path="/connect" size="medium" fullwidth>
-            Buy NFT for {item?.price} ETH
-          </Button>
-        </div>
-      </div>
-    </Modal.Body>
-  </Modal>
+        </Modal.Body>
+      </Modal>
+    ) : null}
+  </div>
 );
 
 export default PlaceBidModal;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Header from '@layout/header';
 import Footer from '@layout/footer';
 import HeroArea from '@containers/hero';
 import ServiceArea from '@containers/services';
-import NewestItmesArea from '@containers/item/newest-item';
+import NewestItemsArea from '@containers/item/newest-item';
 import { useEffect } from 'react';
 import homepageData from '../data/homepages/home-01.json';
 import useAppDispatch from '../store/hooks/useAppDispatch';
@@ -33,7 +33,7 @@ const Home = () => {
       <main id="main-content">
         <HeroArea homeSection={content?.['hero-section']} />
         <ServiceArea data={content?.['service-section']} />
-        <NewestItmesArea data={content?.['newest-section']} items={newItems} />
+        <NewestItemsArea data={content?.['newest-section']} items={newItems} />
       </main>
       <Footer />
     </Wrapper>
