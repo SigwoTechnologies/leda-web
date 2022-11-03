f5bde15608b6e470a11c6524a2997bfb33174e5b
juliswer <julianswer36@gmail.com>
2022-11-03 17:07:25 -0300

    feat(header): disconnect button

--- src/components/user-dropdown/index.tsx
+++ src/components/user-dropdown/index.tsx
@@ -1,41 +1,54 @@
 import Image from 'next/image';
 import Anchor from '@ui/anchor';
 
-const UserDropdown = () => (
-  <div className="icon-box">
-    <Anchor path="/author">
-      <Image
-        src="/images/icons/boy-avater.png"
-        alt="Images"
-        layout="fixed"
-        width={38}
-        height={38}
-      />
-    </Anchor>
-    <div className="rn-dropdown">
-      <div className="rn-inner-top">
-        <h4 className="title">
-          <Anchor path="/author">Jhon Doe</Anchor>
-        </h4>
+const UserDropdown = () => {
+  const handleDisconnectWallet = () => {
+    if (typeof window !== undefined) {
+      localStorage.removeItem('authToken');
+      window.location.reload();
+    }
+  };
+
+  return (
+    <div className="icon-box">
+      <Anchor path="/author">
+        <Image
+          src="/images/icons/boy-avater.png"
+          alt="Images"
+          layout="fixed"
+          width={38}
+          height={38}
+        />
+      </Anchor>
+      <div className="rn-dropdown">
+        <div className="rn-inner-top">
+          <h4 className="title">
+            <Anchor path="/author">Jhon Doe</Anchor>
+          </h4>
+        </div>
+        <div className="add-fund-button mt--20 pb--20">
+          <button
+            className="btn btn-primary-alta w-100"
+            type="button"
+            onClick={handleDisconnectWallet}
+          >
+            Disconnect your Wallet
+          </button>
+        </div>
+        <ul className="list-inner">
+          <li>
+            <Anchor path="/author">My Profile</Anchor>
+          </li>
+          <li>
+            <Anchor path="/edit-profile">Edit Profile</Anchor>
+          </li>
+          <li>
+            <Anchor path="/connect">Manage funds</Anchor>
+          </li>
+        </ul>
       </div>
-      <div className="add-fund-button mt--20 pb--20">
-        <Anchor className="btn btn-primary-alta w-100" path="/connect">
-          Add Your More Funds
-        </Anchor>
-      </div>
-      <ul className="list-inner">
-        <li>
-          <Anchor path="/author">My Profile</Anchor>
-        </li>
-        <li>
-          <Anchor path="/edit-profile">Edit Profile</Anchor>
-        </li>
-        <li>
-          <Anchor path="/connect">Manage funds</Anchor>
-        </li>
-      </ul>
     </div>
-  </div>
-);
+  );
+};
 
 export default UserDropdown;
