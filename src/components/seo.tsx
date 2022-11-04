31168243415aa362e3065059ce38454c5804a65c
juliswer <julianswer36@gmail.com>
2022-11-03 21:51:41 -0300

    fix: pr corrections

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import { BodyWithMetadata } from './metadata-body';
+import { BodyWithoutMetadata } from './nometadata-body';
 
 type Props = {
   pageTitle: string;
@@ -18,53 +20,7 @@ const SEO = ({ pageTitle, pageMeta }: Props) => (
     <meta name="robots" content="noindex, follow" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    {pageMeta ? (
-      <>
-        <meta name="title" content={`${pageMeta.nftName} NFT created by ${pageMeta.nftAuthor}`} />
-        <meta
-          name="description"
-          content={`${pageMeta.nftDescription} - ${pageMeta.nftName} created by ${pageMeta.nftAuthor}. Check that amazing NFT now!`}
-        />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={`${pageMeta.nftName} NFT on LEDA. NFT Marketplace`} />
-        <meta
-          property="og:description"
-          content={`${pageMeta.nftDescription} - ${pageMeta.nftName} created by ${pageMeta.nftAuthor}. Check that amazing NFT now!`}
-        />
-        <meta property="og:image" content={pageMeta.nftImage} />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:title"
-          content={`${pageMeta.nftName} NFT on LEDA. NFT Marketplace`}
-        />
-        <meta
-          property="twitter:description"
-          content={`${pageMeta.nftDescription} - ${pageMeta.nftName} created by ${pageMeta.nftAuthor}. Check that amazing NFT now!`}
-        />
-        <meta property="twitter:image" content={pageMeta.nftImage} />
-      </>
-    ) : (
-      <>
-        <meta name="title" content="LEDA - NFT Marketplace" />
-        <meta name="description" content="LEDA is the largest NFT Marketplace in the earth" />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="LEDA - NFT Marketplace" />
-        <meta
-          property="og:description"
-          content="LEDA is the largest NFT Marketplace in the earth"
-        />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:title" content="LEDA - NFT Marketplace" />
-        <meta
-          property="twitter:description"
-          content="LEDA is the largest NFT Marketplace in the earth"
-        />
-      </>
-    )}
+    {pageMeta ? <BodyWithMetadata pageMeta={pageMeta} /> : <BodyWithoutMetadata />}
   </Head>
 );
 
