8e925924627ab80c2f100aefbc8aba245c65f45a
juliswer <julianswer36@gmail.com>
2022-11-08 14:46:32 -0300

    fix: fixing PR suggestions

--- src/components/user-dropdown/index.tsx
+++ src/components/user-dropdown/index.tsx
@@ -1,8 +1,14 @@
 import Image from 'next/image';
 import Anchor from '@ui/anchor';
 import constants from '../../common/configuration/constants';
+import useAppSelector from '../../store/hooks/useAppSelector';
+import { selectAuthState } from '../../features/auth/store/auth.slice';
+import useMetamask from '../../features/auth/hooks/useMetamask';
 
-const UserDropdown = () => {
+const LogComponent = () => {
+  const { isAuthenticated } = useAppSelector(selectAuthState);
+  const { sign } = useMetamask();
+
   const handleDisconnectWallet = async () => {
     if (typeof window !== undefined) {
       localStorage.removeItem(constants.tokenKey);
@@ -11,10 +17,26 @@ const UserDropdown = () => {
   };
 
   return (
+    <div className="add-fund-button mt--20 pb--20">
+      <button
+        className="btn btn-primary-alta w-100"
+        type="button"
+        onClick={isAuthenticated ? handleDisconnectWallet : sign}
+      >
+        {isAuthenticated ? 'Log out' : 'Log In'}
+      </button>
+    </div>
+  );
+};
+
+const UserDropdown = () => {
+  const { isAuthenticated } = useAppSelector(selectAuthState);
+
+  return (
     <div className="icon-box">
       <Anchor path="/author">
         <Image
-          src="/images/icons/boy-avater.png"
+          src={`/images/icons/${isAuthenticated ? 'boy-avater' : 'unknown-user'}.png`}
           alt="Images"
           layout="fixed"
           width={38}
@@ -24,18 +46,10 @@ const UserDropdown = () => {
       <div className="rn-dropdown">
         <div className="rn-inner-top">
           <h4 className="title">
-            <Anchor path="/author">Jhon Doe</Anchor>
+            <Anchor path="/author">{isAuthenticated ? 'Jhon Doe' : 'Unnamed'}</Anchor>
           </h4>
         </div>
-        <div className="add-fund-button mt--20 pb--20">
-          <button
-            className="btn btn-primary-alta w-100"
-            type="button"
-            onClick={handleDisconnectWallet}
-          >
-            Log out
-          </button>
-        </div>
+        <LogComponent />
         <ul className="list-inner">
           <li>
             <Anchor path="/author">My Profile</Anchor>
