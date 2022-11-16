05a6501efa9b8eb5ab8cf657ed6a45d6975ff128
AlefrankM <alefrank.martinez@stackitgroup.com>
2022-11-15 20:15:41 -0400

    refactor: moving imports

--- src/utils/getContracts.ts
+++ src/utils/getContracts.ts
@@ -1,4 +1,3 @@
-/* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable security/detect-non-literal-require */
 export interface IContractEnvironment {
@@ -8,14 +7,13 @@ export interface IContractEnvironment {
   MarketplaceAbi: never[];
 }
 
+const environment = process.env.NEXT_PUBLIC_NODE_ENV || 'dev';
+const LedaAddress = require(`../contracts/${environment}/LedaNFT-address.json`);
+const LedaAbi = require(`../contracts/${environment}/LedaNFT.json`);
+const MarketplaceAddress = require(`../contracts/${environment}/Marketplace-address.json`);
+const MarketplaceAbi = require(`../contracts/${environment}/Marketplace.json`);
+
 export function getContracts(): IContractEnvironment {
-  const environment = process.env.NEXT_PUBLIC_NODE_ENV || 'dev';
-
-  const LedaAddress = require(`../contracts/${environment}/LedaNFT-address.json`);
-  const LedaAbi = require(`../contracts/${environment}/LedaNFT.json`);
-  const MarketplaceAddress = require(`../contracts/${environment}/Marketplace-address.json`);
-  const MarketplaceAbi = require(`../contracts/${environment}/Marketplace.json`);
-
   return {
     LedaAddress: LedaAddress.address,
     LedaAbi: LedaAbi.abi,
