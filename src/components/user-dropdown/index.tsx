c30dc61e81873583f4896b8d23b7fd012669f17b
juliswer <julianswer36@gmail.com>
2022-11-20 16:16:51 -0300

    fix: prevent user skip terms and services

--- src/components/user-dropdown/index.tsx
+++ src/components/user-dropdown/index.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import Anchor from '@ui/anchor';
+import Modal from 'react-bootstrap/Modal';
+import Button from '@ui/button';
 import constants from '../../common/configuration/constants';
 import useAppSelector from '../../store/hooks/useAppSelector';
 import { selectAuthState } from '../../features/auth/store/auth.slice';
@@ -8,6 +11,15 @@ import useMetamask from '../../features/auth/hooks/useMetamask';
 const LogComponent = () => {
   const { isAuthenticated } = useAppSelector(selectAuthState);
   const { sign } = useMetamask();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isTermsOfServiceChecked, setIsTermsOfServiceChecked] = useState(false);
+
+  const handlePrivacyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsTermsOfServiceChecked(e.target.checked);
+  };
+
+  const handleModalOpen = () => setIsModalOpen((prev) => !prev);
 
   const handleDisconnectWallet = async () => {
     if (typeof window !== undefined) {
@@ -21,10 +33,55 @@ const LogComponent = () => {
       <button
         className="btn btn-primary-alta w-100"
         type="button"
-        onClick={isAuthenticated ? handleDisconnectWallet : sign}
+        onClick={isAuthenticated ? handleDisconnectWallet : handleModalOpen}
       >
         {isAuthenticated ? 'Log out' : 'Log In'}
       </button>
+      <Modal
+        className="rn-popup-modal placebid-modal-wrapper"
+        show={isModalOpen}
+        onHide={handleModalOpen}
+        centered
+      >
+        <button type="button" className="btn-close" aria-label="Close" onClick={handleModalOpen}>
+          <i className="feather-x" />
+        </button>
+        <Modal.Header>
+          <h3 className="modal-title fw-light">
+            <b>Log In LEDA</b>
+          </h3>
+        </Modal.Header>
+        <Modal.Body className="text-center">
+          <span className="text-center">
+            Welcome to Leda Marketplace! Click to sign in and accept the Leda Terms of Service. This
+            request will not trigger a blockchain transaction or cost any gas fees. Your
+            authentication status will reset after 24 hours.
+          </span>
+          <div className="my-5 text-center">
+            <input
+              type="checkbox"
+              className="rn-check-box-input"
+              id="terms"
+              onChange={handlePrivacyChange}
+              checked={isTermsOfServiceChecked}
+            />
+            <label className="rn-check-box-label" htmlFor="terms">
+              I agree to the <Anchor path="/terms-condition">Terms of service</Anchor>{' '}
+            </label>
+          </div>
+          <div className="d-flex align-items-center justify-content-center">
+            <Button
+              disabled={!isTermsOfServiceChecked}
+              onClick={() => {
+                sign();
+                handleModalOpen();
+              }}
+            >
+              Log In
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
