448cdc26e79c43e0308388d9fd9b16adfdc3e369
AlefrankM <alefrank.martinez@stackitgroup.com>
2022-12-02 06:01:25 -0400

    refactor: hide item

--- src/components/item-details/title.tsx
+++ src/components/item-details/title.tsx
@@ -3,11 +3,12 @@ import { useMemo } from 'react';
 import ItemStatus from '../../common/minting/enums/item-status.enum';
 import { selectLikedItems } from '../../features/account/store/account.slice';
 import { withAuthProtection } from '../../features/auth/store/auth.actions';
-import { hideItem, likeItem } from '../../features/marketplace/store/marketplace.actions';
+import { likeItem } from '../../features/marketplace/store/marketplace.actions';
 import { selectIsOwner } from '../../features/marketplace/store/marketplace.slice';
 import useAppDispatch from '../../store/hooks/useAppDispatch';
 import useAppSelector from '../../store/hooks/useAppSelector';
 import ShareDropdown from '../share-dropdown';
+import { HideItemButton } from './hide-item-button';
 
 type Props = {
   className?: string;
@@ -20,10 +21,6 @@ const ProductTitle = ({ className }: Props) => {
   } = useAppSelector((state) => state.marketplace);
 
   const isOwner = useAppSelector(selectIsOwner);
-
-  const handleHideItem = () => {
-    dispatch(withAuthProtection(hideItem(itemId)));
-  };
 
   const handleLikeItem = () => {
     dispatch(withAuthProtection(likeItem(itemId)));
@@ -41,7 +38,7 @@ const ProductTitle = ({ className }: Props) => {
     <div className={clsx('pd-title-area', className)}>
       <div>
         <span style={{ fontStyle: 'italic', color: 'orange', fontWeight: 500 }}>
-          {status === ItemStatus.Hidden && 'This item is hidden'}
+          {status === ItemStatus.Hidden && isOwner && 'This item is hidden'}
         </span>
 
         <h4 className="title">
@@ -49,20 +46,7 @@ const ProductTitle = ({ className }: Props) => {
         </h4>
       </div>
       <div className="pd-react-area">
-        {isOwner && (
-          <div className="count">
-            <button
-              type="button"
-              style={{ width: '5rem', fontSize: '20px' }}
-              onClick={handleHideItem}
-              title={`currently is ${status === ItemStatus.Hidden ? 'hidden' : 'visible'}`}
-            >
-              <i
-                className={clsx(status === ItemStatus.Hidden ? 'feather-eye-off' : 'feather-eye')}
-              />
-            </button>
-          </div>
-        )}
+        {isOwner && <HideItemButton />}
 
         <div className="count">
           <button type="button" className={`${likeClassName} heart-count`} onClick={handleLikeItem}>
